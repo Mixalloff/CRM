753946393b69daee7dbe151ebddb84497eabaf14
Nikita Matrosov <matrosov.nikita94@yandex.ru>
2015-07-17 22:56:49 +0300

    user

--- html/reactClasses/users.js
+++ html/reactClasses/users.js
@@ -7,23 +7,23 @@ var User = React.createClass({
 	},
 	render: function() {
 		return(
-				<div className="col-md-2">
-						<div className="profile-border">
-							<div className="profile">
-								<i className="fa fa-user"></i>
-							</div>
-							<div className="user-name"> 
-								{this.state.name}
-							</div>
-							<div className="post">
-									{this.state.post}
-							</div>
-						</div>
-						<div className="pass">
-							<input type="password" placeholder="Пароль"/>
-							<i className="fa fa-lock"></i>
-						</div>
-					</div>
+			<div className="col-md-2">
+			<div className="profile-border">
+			<div className="profile">
+			<i className="fa fa-user"></i>
+			</div>
+			<div className="user-name"> 
+			{this.state.name}
+			</div>
+			<div className="post">
+			{this.state.post}
+			</div>
+			</div>
+			<div className="pass">
+			<input type="password" placeholder="Пароль"/>
+			<i className="fa fa-lock"></i>
+			</div>
+			</div>
 			);
 	}
 });
@@ -35,20 +35,20 @@ var Users = React.createClass({
 	},
 	render: function() {
 		return(
-		<div>
-		<div className="name-department">
-					{this.state.department}
-		</div>
+			<div>
+			<div className="name-department">
+			{this.state.department}
+			</div>
 
-		<div className="profiles">
+			<div className="profiles">
 			<User/>
 			<User/>
 			<User/>
 			<User/>
 			<User/>
-		</div>
-		</div>
-		);
+			</div>
+			</div>
+			);
 	}
 });
 React.render(<Users/>, document.getElementById('user-groups'));
