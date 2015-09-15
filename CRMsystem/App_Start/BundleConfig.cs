0b16055de97f0c110d460351540503da00a25862
matrosovN <matrosov.nikita94@gmail.com>
2015-09-15 16:59:15 +0300

    Добавлен иконочный шрифт и сгруппированы бандлы

--- CRMsystem/App_Start/BundleConfig.cs
+++ CRMsystem/App_Start/BundleConfig.cs
@@ -8,47 +8,24 @@ namespace CRMsystem
         //Дополнительные сведения об объединении см. по адресу: http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/owljs").Include(
-                        "~/Scripts/owl.carousel.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
-                        "~/Scripts/jquery-ui.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/myscripts").Include(
-                      "~/js/*.js"));
-
-
-            // Используйте версию Modernizr для разработчиков, чтобы учиться работать. Когда вы будете готовы перейти к работе,
-            // используйте средство сборки на сайте http://modernizr.com, чтобы выбрать только нужные тесты.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
+            bundles.Add(new ScriptBundle("~/bundles/plugins").Include(
+                        "~/Content/Scripts/jquery-{version}.js",
+                        "~/Content/Scripts/owl.carousel.min.js",
+                        "~/Content/Scripts/jquery.validate*",
+                        "~/Content/js/*.js",
+                        "~/Content/Scripts/jquery-ui.min.js",
+                        "~/Content/Scripts/modernizr-*",
+                        "~/Content/Scripts/bootstrap.js",
+                        "~/Content/Scripts/respond.js",
+                         "~/Content/react/*.js"
+                        ));
+      
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/font-awesome.min.css"));
-
-            bundles.Add(new StyleBundle("~/Content/jquery-ui").Include(
-                    "~/Content/jquery-ui.min.css"));
-
-            bundles.Add(new StyleBundle("~/Content/owlcss").Include(
-                   "~/Content/owl.carousel.css"));
-
-            bundles.Add(new ScriptBundle("~/bundles/react").Include(
-                      "~/react/*.js"));
-
-            bundles.Add(new StyleBundle("~/Content/style/css").Include(
-            "~/Content/style/*.css"));
-
+                      "~/Content/fonts/font-awesome-4.4.0/css/font-awesome.min.css",
+                      "~/Content/jquery-ui.min.css",
+                       "~/Content/owl.carousel.css"
+                      ));
         }
     }
 }
